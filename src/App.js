29a314cb5e3fde2f56d4bfea2ad123963646aad7
Moon Bui <buinm@miamioh.edu>
2021-12-29 16:16:35 -0600

    reorganized files, changed method of linking image

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import "./App.css";
 import mergeImages from "merge-images";
 import React, { useState, useEffect, useRef } from "react";
-import blue50 from "./alpaca/backgrounds/blue50.png";
-import flower from "./alpaca/accessories/flower.png";
-import eyes from "./alpaca/eyes/default.png";
-import ears from "./alpaca/ears/default.png";
-import hair from "./alpaca/hair/default.png";
-import leg from "./alpaca/leg/default.png";
-import mouth from "./alpaca/mouth/default.png";
-import neck from "./alpaca/neck/default.png";
-import nose from "./alpaca/nose.png";
+
 
 const Alpaca = () => {
 
   // mergeImages([blue50, flower]).then(b64 => document.querySelector("img").src = b64);
   mergeImages([
-    { src: blue50 }, 
-    { src: neck }, 
-    { src: nose }, 
-    { src: mouth }, 
-    { src: leg },  
-    { src: ears }, 
-    { src: hair }, 
-    { src: eyes }, 
-    { src: flower }
+    { src: "alpaca/backgrounds/blue50.png" }, 
+    { src: "alpaca/neck/default.png" }, 
+    { src: "alpaca/nose.png" }, 
+    { src: "alpaca/mouth/default.png" }, 
+    { src: "alpaca/leg/default.png" },  
+    { src: "alpaca/ears/default.png" }, 
+    { src: "alpaca/hair/default.png" }, 
+    { src: "alpaca/eyes/default.png" }, 
+    { src: "alpaca/accessories/flower.png" }
   ]).then(b64 => document.querySelector('img').src = b64);
 
   return (
